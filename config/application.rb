14de8e783817e27d602a56d3e19d8b98f5360b3d
Carmen Chung <103980536+carmenintech@users.noreply.github.com>
2023-02-03 21:54:42 +1100

    Don't compress CSS ?

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MyApp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+
+    config.assets.css_compressor = nil
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
